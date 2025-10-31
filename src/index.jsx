2a514728d79f1a2920d77485306bbe872928d5cf
Todor Yadkov <todor.yadkov@gmail.com>
2025-10-31 13:50:26 +0200

    Add main functionality

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "../src/styles/index.scss";
 
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider as ReduxProvider } from "react-redux";
+import { ErrorBoundary } from "react-error-boundary";
+
+import { store } from "./store";
+
+import App from "./App.jsx";
+import { ErrorBoundaryComponent } from "./pages";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary
+      FallbackComponent={ErrorBoundaryComponent}
+      onReset={() => {
+        console.warn("Error boundary reset!");
+      }}
+    >
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
+    </ErrorBoundary>
   </StrictMode>,
-)
+);
